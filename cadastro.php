026e202d96d2abfb8342370dc265ec0c1d249a3f
Kelvin <kelvinnicolau@gmail.com>
2021-04-18 03:28:30 -0300

    Iniciando Projeto

---
+++ cadastro.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>SISTEMA</title>
+        
+        <!-- Bootstrap -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="assets/css/index.css">
+
+    </head>
+    <body>
+        <section id="titulo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1>CADASTRAR PRODUTO</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+
+        
+    </body>
+
+</html>
